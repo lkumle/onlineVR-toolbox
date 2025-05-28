7ae36becadd6c9a9bbf215230789e695234fc658
levi.kumle <levi.kumle@gmail.com>
2025-05-28 19:14:41 +0100

    initial commit

---
+++ unityPackage/onlineVR/README.md
@@ -0,0 +1,34 @@
+# About onlineVR
+
+Use onlineVR to enable data transfer functionalities with a custom web application. The resources presented here are part of the [onlineVR-toolbox](https://github.com/lkumle/onlineVR-toolbox) and accompanying [tutorials](https://lkumle.github.io/onlineVRtoolbox_tutorials/). 
+
+# Installing onlineVR
+
+To install this package, follow the instructions in the [Package Manager documentation](https://lkumle.github.io/onlineVRtoolbox_tutorials/docs/unity/integrate.html).
+
+# Using Sprite Editor
+
+The onlineVR Manual can be found [here](https://lkumle.github.io/onlineVRtoolbox_tutorials/).
+
+
+# Technical details
+## Requirements
+
+This version of Sprite Editor is compatible with the following versions of the Unity Editor:
+
+* 2022.3 and later (recommended)
+
+## Package contents
+
+The following table indicates the folder structure of the Sprite package:
+
+|Location|Description|
+|---|---|
+|`<Editor>`|Root folder containing the ConnectionHandler Prefab.|
+
+
+## Document revision history
+
+|Date|Reason|
+|---|---|
+|May 25, 2025|Document created. Matches package version 1.0.0|
---
+++ webApp/bottle_app.py
@@ -0,0 +1,97 @@
+# ================================================================ #
+#  Template for a Simple Data Collection Server (Bottle Framework) #
+# ================================================================ #
+#
+# This script provides a minimal server implementation for researchers who need
+# a simple way to allocate unique IDs and collect data files from remote sources.
+# It is designed to work with Unity applications and can be customized for specific research needs.
+#
+# Usage:
+# - follow step-by-step guide on how to set up server!
+#    --> https://lkumle.github.io/onlineVRtoolbox_tutorials/docs/webApplication/Index.html
+# - Configure `filename_length` to match Unity's filename encoding.
+#
+# - The server allocates unique IDs via a GET request.
+# - Data files are received via a PUT request and stored locally.
+#
+# Author:  Alfie Brasier & Levi Kumle 
+# Institution: Department of Psychology, University of Oxford
+# Contact: levi.kumle@psy.ox.ac.uk
+# Date: 2025-03-04
+# =========================================================== #
+
+from bottle import Bottle, request
+
+# ================================== #
+# ================================== #
+# Configure: enter lengths of file name here
+# --> needs to match Unity's filename lengths
+filename_length = 41
+# ================================== #
+
+
+# =============================================== #
+# # Prepare
+# =============================================== #
+# Create a Bottle application instance
+application = Bottle()
+
+# function  to allocate a unique ID
+def get_next_id():
+    """
+    Reads the current ID from a file, increments it, and updates the file.
+    Returns the allocated ID.
+    """
+    id_file_path = './mysite/id.txt'
+    
+    # Read the current ID from the file
+    with open(id_file_path, 'r') as file:
+        current_id = int(file.read().strip())
+        print(f'Current ID: {current_id}')
+    
+    # Increment the ID and write it back to the file
+    with open(id_file_path, 'w') as file:
+        file.write(str(current_id + 1))
+    
+    return current_id
+
+
+# =============================================== #
+# # Define a GET endpoint to allocate a unique ID
+# =============================================== #
+@application.get('/')
+def welcome():
+    """
+    Handles GET requests. Allocates a unique ID and returns a response.
+    """
+    # get updated ID
+    allocated_id = get_next_id()
+    
+    # return response
+    # ensures the allocated_id is displayed as a 6-digit number, padded with leading zeros if necessary.
+    response_message = f'Connection established. Allocated ID: {allocated_id:06d}'
+    return response_message
+
+
+# =============================================== #
+# Define a PUT endpoint to receive and store data
+# =============================================== #
+@application.put('/')
+def save_data():
+    """
+    Handles PUT requests. Extracts a user ID/file name from the request body and saves the
+    remaining data into a file named after the user ID.
+    """
+    content = request.body.read()
+    
+    # Extract the first 41 bytes as the filenm (assuming UTF-8 encoding)
+    filename = content[:filename_length].decode('UTF-8')
+    
+    # Define the file path for saving data
+    file_path = f'./mysite/files/{filename}.bytes'
+    
+    # Write the remaining content to the file
+    with open(file_path, 'wb') as file:
+        file.write(content[filename_length:])
+    
+    return "Upload complete."
